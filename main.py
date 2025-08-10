78fe7de82dd41762509ae196f255a20d54632ce6
TMLY12 <yurecmen23@gmail.com>
2025-08-10 17:21:56 +0200

    10.08 commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-print("Hello World")
-s ="12"
-p = 12
-print("hello", end=' ')
-print(p, end='' )
-print("+"+s+"=", end='')
-print(p+int(s), end='' )
+price_per_croissant = 1.04
+price_per_glass = 0.34
+price_per_coffee_pack = 4.42
+num_croissants = int(input("Введіть кількість круасанів: "))
+num_glasses = int(input("Введіть кількість склянок: "))
+num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
+total_cost = num_croissants * price_per_croissant + \
+             num_glasses * price_per_glass + \
+             num_coffee_packs * price_per_coffee_pack
+total_dollars = int(total_cost)
+total_cents = int(total_cost * 100)
+print(f"Загальна вартість у повних доларах: {total_dollars} доларів")
+print(f"Загальна вартість у центах: {total_cents} центів")15
